584da00528931ee2e279e13ff6c9aa7486e2f2f5
yuheng Chen <e31716@gmail.com>
2020-08-24 22:41:19 +0800

    change node return

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // res.send(JSON.stringify({ Hello: 'World' }))
+    res.sendfile(__dirname + "/views/index.html");
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
