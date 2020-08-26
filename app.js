0f14d95bad2c376a2c111536227acad8d68f3f7c
Elek Chen <elek.chen@gjsoft.net>
2020-08-26 22:35:55 +0800

    comment unused code

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/countdown', function (req, res) {
-    var img = convertImgToBase64();
-    console.log(img)
-    res.writeHead(200, {
-        'Content-Type': 'image/gif',
-        'Content-Length': img.length,
-        'cache-control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-    });
-    res.end('data:image/gif;base64,' + img);
-});
+// app.get('/countdown', function (req, res) {
+//     var img = convertImgToBase64();
+//     console.log(img)
+//     res.writeHead(200, {
+//         'Content-Type': 'image/gif',
+//         'Content-Length': img.length,
+//         'cache-control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+//     });
+//     res.end('data:image/gif;base64,' + img);
+// });
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
