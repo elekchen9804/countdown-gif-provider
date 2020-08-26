57f8e5b95f2ca6bedf8c241837e6784207f8c2d8
Elek Chen <elek.chen@gjsoft.net>
2020-08-26 22:20:10 +0800

    convert time format

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { generateGif } = require('./modules/countdown-gif')
+const { generateGif, convertImgToBase64 } = require('./modules/countdown-gif')
 const app = express()
 const port = process.env.PORT || 4000;
 // Static image allow
@@ -7,8 +7,19 @@ app.use(express.static('data'))
 
 app.get('/', (req, res) => {
     generateGif()
-    res.sendfile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
+
+app.get('/countdown', function (req, res) {
+    var img = convertImgToBase64();
+    console.log(img)
+    res.writeHead(200, {
+        'Content-Type': 'image/gif',
+        'Content-Length': img.length,
+        'cache-control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    });
+    res.end('data:image/gif;base64,' + img);
+});
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
--- modules/countdown-gif/index.js
+++ modules/countdown-gif/index.js
@@ -1,22 +1,24 @@
 const GIFEncoder = require('gifencoder');
+const prettyMs = require('pretty-ms');
 const dayjs = require('dayjs');
-const duration = require('dayjs/plugin/duration')
-dayjs.extend(duration)
+const duration = require('dayjs/plugin/duration');
+dayjs.extend(duration);
 
 const { createCanvas } = require('canvas');
 const fs = require('fs');
 const path = require('path');
 // app root path
 const appDir = path.dirname(require.main.filename);
+const imagePath = appDir + '/data/myanimated.gif';
 
 module.exports = {
     generateGif: (width = 300, height = 100) => {
         const currentDateTime = dayjs();
-        const endDateTime = dayjs('2020-08-26T02:59:59.000Z');
-        const dateDiff = endDateTime.diff(currentDateTime, 'minute')
+        const endDateTime = dayjs('2020-12-31T23:59:59.000Z');
+        let dateDiff = endDateTime.diff(currentDateTime, 'ms');
         const encoder = new GIFEncoder(width, height);
         // stream the results as they are available into myanimated.gif
-        encoder.createReadStream().pipe(fs.createWriteStream(appDir + '/data/myanimated.gif'));
+        encoder.createReadStream().pipe(fs.createWriteStream(imagePath));
 
         encoder.start();
         encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
@@ -32,16 +34,30 @@ module.exports = {
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
 
-        for (let i = 20; i >= 0; i--) {
+        for (let i = 60; i >= 0; i--) {
+            // Convert time format
+            let formattedTime = prettyMs(dateDiff, { secondsDecimalDigits: 0 });
+
             // paint BG
             ctx.fillStyle = '#fff';
             ctx.fillRect(0, 0, width, height);
             // paint text
             ctx.fillStyle = '#000';
-            ctx.fillText(`${i} s`, 150, 50);
+            ctx.fillText(`${formattedTime}`, 150, 50);
             encoder.addFrame(ctx);
+            dateDiff = dateDiff - 1000;
         }
 
         encoder.finish();
+    },
+    convertImgToBase64: () => {
+        return base64_encode(imagePath);
+
+        function base64_encode(file) {
+            // read binary data
+            var bitmap = fs.readFileSync(file);
+            // convert binary data to base64 encoded string
+            return new Buffer.from(bitmap).toString('base64');
+        }
     }
 }
